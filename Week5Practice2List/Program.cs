a3aba5a2e3fd873634739166c5293b6aaba7a095
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-08-14 15:36:10 +0300

    Add project files.

---
+++ Week5Practice2List/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> list = new List<string>() { "1-Bülent Ersoy", "2-Ajda Pekkan", "3-Ebru Gündeş", "4-Hadise", "5-Hande Yener", "6-Tarkan", "7-Funda Arar", "8-Demet Akalın" };
+        Console.WriteLine("**Davetliler**");
+
+        foreach (string s in list)
+        {
+            Console.WriteLine(s);
+        }
+       
+
+    }
+}
